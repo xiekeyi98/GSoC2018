623f613439c7ff9f42ae17830647b04f20c719db
Keyi Xie <xiekeyi98@vip.qq.com>
2018-02-20 14:38:43 -0800

    Reconfigure segmentree with very simple test

--- ProgrammingTest/segment_tree/segment_tree.hpp
+++ ProgrammingTest/segment_tree/segment_tree.hpp
@@ -4,12 +4,8 @@
 #include<vector>
 #include<algorithm>
 
-#ifdef DEBUG
-using namespace std ;
-#endif // DEBUG 
 namespace st
 {
-	static double INFINITE = 0xFFFFFFFFFFFFFFFF;
 	template<typename T> struct node
 	{
 		T value;
@@ -22,12 +18,8 @@ namespace st
 		class basic_operator
 		{
 			public:
-				virtual void pushup( std::vector<T> &segtree , int root ) = 0 ;
-				// 1 is to make a segment to be the number ;
-				// 2 is to make a segment to add the number; TODO 2 is unnecessary;
-				virtual void pushdown1( std::vector<T> &segtree,int root ) = 0 ; 
-				//virtual void pushdown2( std::vector<T> &segtree,int root ) = 0 ; 
-			protected:
+				virtual void pushup( std::vector<node<T>> &segtree , int root ) = 0 ;
+				virtual void pushdown1( std::vector<node<T>> &segtree,int root ) = 0 ; 
 				int left_child( int root ) const { return root * 2 + 1 ; } ;
 				int right_child(int root ) const { return root * 2 + 2 ; } ; 
 		};
@@ -36,43 +28,44 @@ namespace st
 		class Max: public basic_operator<T> 
 	{
 		public:
+			const static int INFINITE = -0x7fffffff;
 			T operator() ( const T &a , const T &b ) const 
 			{ 
-				if( a!= INFINITE && b != INFINITE )
-					return std::max(a,b);
+				if( a != INFINITE && b != INFINITE )
+					return std::max(a,b) ; 
 				else if( a == INFINITE && b != INFINITE )
 					return b ;
 				else if( a != INFINITE && b == INFINITE )
 					return a ;
 				else if( a == INFINITE && b == INFINITE )
-					throw " two INFINITE! " ; 
+					throw " two INFINITE! " ; // TODO 
+				return INFINITE ; 
+			}
+			T _Max( const T &a , const T &b ) const 
+			{ 
+				if( a != INFINITE && b != INFINITE )
+					return std::max(a,b) ; 
+				else if( a == INFINITE && b != INFINITE )
+					return b ;
+				else if( a != INFINITE && b == INFINITE )
+					return a ;
+				else if( a == INFINITE && b == INFINITE )
+					throw " two INFINITE! " ; // TODO 
+				return INFINITE ; 
 			}
 
 			void pushup( std::vector<node<T>> &segtree , int root ) 
 			{
 				segtree[root].value = 
-					Max( segtree[this->left_child(root)].value , segtree[this->right_child(root)].value);
+					_Max( segtree[this->left_child(root)].value ,
+							segtree[this->right_child(root)].value);
 				segtree[root].l = segtree[this->left_child(root)].l;
 				segtree[root].r = segtree[this->right_child(root)].r;
 			}
 
 			void pushdown1(std::vector<node<T>> &segtree , int root ) 
 			{
-				if( segtree[root]._lazytag)
-				{
-					segtree[this->left_child(root)].lazytag = segtree[root].lazytag;
-					segtree[this->left_child(root)].value = segtree[root].lazytag ;
-					segtree[this->right_child(root)].lazytag = segtree[root].lazytag;
-					segtree[this->right_child(root)].value = segtree[root].lazytag ;
-					segtree[root]._lazytag = false; 
-					segtree[root].lazytag = 0 ; 
-					// 0 is not a good INFINITE number , but I don't know what to do better .  
-				}
-			}
-
-			void pushdown2(std::vector<node<T>> &segtree , int root ) 
-			{
-				if( segtree[root]._lazytag)
+				if( segtree[root]._lazytag && segtree[root].r - segtree[root].l > 1 )
 				{
 					segtree[this->left_child(root)].lazytag += segtree[root].lazytag;
 					segtree[this->left_child(root)].value += segtree[root].lazytag ;
@@ -80,6 +73,7 @@ namespace st
 					segtree[this->right_child(root)].value += segtree[root].lazytag ;
 					segtree[root]._lazytag = false; 
 					segtree[root].lazytag = 0 ; 
+					// TODO 0 is not a good INFINITE number , but I don't know what to do better .  
 				}
 			}
 	};
@@ -88,6 +82,7 @@ namespace st
 		class Min: public basic_operator<T> 
 	{
 		public:
+			const static int INFINITE = 0x7fffffff;
 			T operator() ( const T &a , const T &b ) const 
 			{ 
 				if( a!= INFINITE && b != INFINITE )
@@ -98,11 +93,24 @@ namespace st
 					return a ;
 				else if( a == INFINITE && b == INFINITE )
 					throw " two INFINITE! " ; 
+				return INFINITE ; 
+			}
+			T _Min( const T &a , const T &b ) const 
+			{ 
+				if( a!= INFINITE && b != INFINITE )
+					return std::min(a,b);
+				else if( a == INFINITE && b != INFINITE )
+					return b ;
+				else if( a != INFINITE && b == INFINITE )
+					return a ;
+				else if( a == INFINITE && b == INFINITE )
+					throw " two INFINITE! " ; 
+				return INFINITE ; 
 			}
 			void pushup( std::vector<node<T>> &segtree , int root ) 
 			{
 				segtree[root].value = 
-					Min( segtree[this->left_child(root)].value , 
+					_Min( segtree[this->left_child(root)].value , 
 							segtree[this->right_child(root)].value);
 				segtree[root].l = segtree[this->left_child(root)].l;
 				segtree[root].r = segtree[this->right_child(root)].r;
@@ -110,20 +118,7 @@ namespace st
 
 			void pushdown1(std::vector<node<T>> &segtree , int root ) 
 			{
-				if( segtree[root]._lazytag)
-				{
-					segtree[this->left_child(root)].lazytag = segtree[root].lazytag;
-					segtree[this->left_child(root)].value = segtree[root].lazytag ;
-					segtree[this->right_child(root)].lazytag = segtree[root].lazytag;
-					segtree[this->right_child(root)].value = segtree[root].lazytag ;
-					segtree[root]._lazytag = false; 
-					segtree[root].lazytag = 0 ; 
-				}
-			}
-
-			void pushdown2(std::vector<node<T>> &segtree , int root ) 
-			{
-				if( segtree[root]._lazytag)
+				if( segtree[root]._lazytag && segtree[root].r - segtree[root].r > 1 )
 				{
 					segtree[this->left_child(root)].lazytag += segtree[root].lazytag;
 					segtree[this->left_child(root)].value += segtree[root].lazytag ;
@@ -133,12 +128,14 @@ namespace st
 					segtree[root].lazytag = 0 ; 
 				}
 			}
+
 	};
 
 	template< typename T > 
 		class Plus: public basic_operator<T> 
 	{
 		public:
+			const static int INFINITE = 0 ; 
 			T operator() ( const T &a , const T &b ) const 
 			{
 				if( a!= INFINITE && b != INFINITE )
@@ -148,12 +145,25 @@ namespace st
 				else if( a != INFINITE && b == INFINITE )
 					return a ;
 				else if( a == INFINITE && b == INFINITE )
-					throw " two INFINITE! " ; 
+					;
+				return INFINITE ; 
+			}
+			T _Plus( const T &a , const T &b ) const 
+			{
+				if( a!= INFINITE && b != INFINITE )
+					return a+b;
+				else if( a == INFINITE && b != INFINITE )
+					return b ;
+				else if( a != INFINITE && b == INFINITE )
+					return a ;
+				else if( a == INFINITE && b == INFINITE )
+					;
+				return INFINITE ; 
 			}
 			void pushup( std::vector<node<T>> &segtree , int root ) 
 			{
 				segtree[root].value = 
-					Plus( segtree[this->left_child(root)].value , 
+					_Plus( segtree[this->left_child(root)].value , 
 							segtree[this->right_child(root)].value);
 				segtree[root].l = segtree[this->left_child(root)].l;
 				segtree[root].r = segtree[this->right_child(root)].r;
@@ -161,35 +171,16 @@ namespace st
 
 			void pushdown1(std::vector<node<T>> &segtree , int root ) 
 			{
-				if( segtree[root]._lazytag)
-				{
-					segtree[this->left_child(root)].lazytag = segtree[root].lazytag;
-					// solve the closed interval ;
-					segtree[this->left_child(root)].value = 
-						segtree[root].lazytag * 
-						(segtree[this->left_child(root)].r - 
-						 segtree[this->left_child(root)].l + 1 ) ;
-					segtree[this->right_child(root)].lazytag = segtree[root].lazytag;
-					segtree[this->right_child(root)].value =
-						segtree[root].lazytag * 
-						(segtree[this->right_child(root)].r - 
-						 segtree[this->right_child(root)].l + 1 ) ;
-					segtree[root]._lazytag = false; 
-					segtree[root].lazytag = 0 ; 
-				}
-			}
-
-			void pushdown2(std::vector<node<T>> &segtree , int root ) 
-			{
-				if( segtree[root]._lazytag)
+				if( segtree[root]._lazytag && segtree[root].r - segtree[root].l > 1 )
 				{
 					segtree[this->left_child(root)].lazytag += segtree[root].lazytag;
+					// solve the closed interval ;
 					segtree[this->left_child(root)].value += 
 						segtree[root].lazytag * 
 						(segtree[this->left_child(root)].r - 
 						 segtree[this->left_child(root)].l + 1 ) ;
 					segtree[this->right_child(root)].lazytag += segtree[root].lazytag;
-					segtree[this->right_child(root)].value += 
+					segtree[this->right_child(root)].value +=
 						segtree[root].lazytag * 
 						(segtree[this->right_child(root)].r - 
 						 segtree[this->right_child(root)].l + 1 ) ;
@@ -197,56 +188,34 @@ namespace st
 					segtree[root].lazytag = 0 ; 
 				}
 			}
+
 	};
 	template <typename T1 , typename T2 > class segment_tree
 	{
 		public:
 			segment_tree() {};
-			~segment_tree(){ segtree.resize(0);}// maybe this is unnecessary?
-			void build( std::vector<T1> const & a , basic_operator<T2> _operator , int root , int l , int r );
-			T1 query( int l , int r ) { query( 0 , l , r ) ; } ; 
-			void modify( int l , int r , T1 newmark ) {  modify( 0 , l , r , newmark ) ; } ;  
+			~segment_tree(){ segtree.resize(0);}// TODO maybe this is unnecessary?
+			void build( std::vector<T1> const & a , int root , int l , int r );
+			T1 query( int l , int r ) { return query( 0 , l , r ) ; } ; 
+			void modify1( int l , int r , T1 newmark ) {  modify1( 0 , l , r , newmark ) ; } ;  
 
 		private:
 			T1 query( int root , int l , int r ) ;
-			void modify( int root , int l , int r , T1 newmark ) ;
+			void modify1( int root , int l , int r , T1 newmark ) ;
+			void modify2( int root , int l , int r , T1 newmark ) ;
 			std::vector< node<T1> > segtree ; 
-			basic_operator<T2> _operator ; 
+			T2 _operator ; 
 			bool uninit= true ; 
-			//int INFINITE = -0x7fffffff;  //TODO this is not a good number . 
-			//		inline int left_child(int root) const { return root * 2 + 1 ; };
-			//		inline int right_child(int root)const { return root * 2 + 2 ; } ; 
-			//		void pushup( int root )
-			//		{
-			//			segtree[root].value = 
-			//				std::max( segtree[left_child(root)].value , segtree[right_child(root)].value);
-			//			segtree[root].l = segtree[left_child(root)].l;
-			//			segtree[root].r = segtree[right_child(root)].r;
-			//		}
-
-			//		void pushdown( int root )
-			//		{
-			//			if( segtree[root].lazytag )
-			//			{
-			//				segtree[left_child(root)].lazytag = segtree[root].lazytag;
-			//				segtree[left_child(root)].value = segtree[root].lazytag ;
-			//				segtree[right_child(root)].lazytag = segtree[root].lazytag;
-			//				segtree[right_child(root)].value = segtree[root].lazytag ;
-			//				segtree[root].lazytag = 0 ; 
-			//			}
-
-			//		}
 	};
 	template<typename T1,typename T2 > 
 		void segment_tree<T1,T2>::
-		build( std::vector<T1> const &a , basic_operator<T2> _operator ,int root , int l , int r )  
+		build( std::vector<T1> const &a , int root , int l , int r )  
 		{
 			if( l > r )
 				throw "sorry,l must not more than r " ; 
 			if( uninit )
 			{
 				segtree.resize( 4 * ( r - l ) ) ;
-				_operator = this->_operator;
 				uninit = false ;
 			}
 
@@ -266,10 +235,9 @@ namespace st
 					segtree[root].lazytag = 0 ; 
 					segtree[root]._lazytag = false ; 
 					int mid = ( l + r ) / 2 ; 
-					build( a , _operator , _operator.left_child(root) , l , mid ) ;
-					build( a , _operator , _operator.right_child(root) , mid + 1 , r ) ; 
+					build( a , _operator.left_child(root) , l , mid ) ;
+					build( a , _operator.right_child(root) , mid + 1 , r ) ; 
 					_operator.pushup(segtree,root);
-					//pushup(root);
 				}
 			}
 		}
@@ -282,36 +250,36 @@ namespace st
 			if( segtree[root].l >= l && segtree[root].r <= r )
 				return segtree[root].value;
 			else if( segtree[root].l > r || segtree[root].r < l )
-				return INFINITE ;
+				return _operator.INFINITE ;
 			else 
 			{
-				//pushdown(root);
 				_operator.pushdown1(segtree,root);
-				return _operator( query( _operator.left_child(root), l , r) , 
-						query(_operator.right_child(root), l , r ));
-				//pushup(root);
+				T1 ans = _operator( query( _operator.left_child(root), l , r) , 
+						query(_operator.right_child(root), l , r ) ) ;
 				_operator.pushup(segtree,root);
+				return ans ; 
 			}
 		}
 	template<typename T1,typename T2>
-		void segment_tree<T1,T2>::modify( int root , int l , int r , T1	newmark)
+		void segment_tree<T1,T2>::modify1( int root , int l , int r , T1 newmark)
 		{
 			if( l > r )
 				std::swap(l,r);
 			if( segtree[root].l >= l && segtree[root].r <= r )
 			{
 				segtree[root]._lazytag = true ; 
-				segtree[root].lazytag = newmark;
-				segtree[root].value = newmark;
+				segtree[root].lazytag += newmark;
+				segtree[root].value += newmark;
+				return ; 
 			}
-
 			else if( segtree[root].l > r || segtree[root].r < l )
 				return ;
-			else 
-			{
-				modify( _operator.left_child(root) , l , r , newmark ) ;
-				modify( _operator.right_child(root) , l , r , newmark);
-			}
+
+			_operator.pushdown1( segtree , root ) ; 
+
+			modify1( _operator.left_child(root) , l , r , newmark ) ;
+			modify1( _operator.right_child(root) , l , r , newmark);
+			_operator.pushup(segtree,root);
 		}
 };
 #endif //segment_tree_hpp
--- ProgrammingTest/segment_tree/test.cpp
+++ ProgrammingTest/segment_tree/test.cpp
@@ -10,21 +10,39 @@ int main()
 	{
 		a.push_back(i);
 	}
-	segment_tree<int> st;
-	st.build( a, 0 , 0 , 9) ;
-	cout << st.query( 0 , 0 , 0 ) << endl ;
-	cout << st.query( 0 , 1 , 1 ) << endl ;
-	cout << st.query( 0 , 0 , 9)  << endl ;
-	cout << st.query( 0 , 8 , 9) << endl ; 
-	cout << st.query( 0 , 1 , 5 ) << endl ;
-	st.modify( 0 , 1 , 5, 100 ) ;
-	cout << st.query( 0 , 1 , 5 ) << endl ;
-	st.modify( 0 , 1 , 5 , -10 );
-	st.modify( 0 , 0 , 9 , 1 ) ;
-	cout << st.query( 0 , 0 , 9 ) << endl ;
-
-	cout << st.query( 0 , 5 , 1 ) << endl ; 
-
+//	segment_tree< int , Max<int> > st ;
+//	st.build( a , 0 , 0 , 9 ) ;
+//	cout << st.query( 0 , 0 ) << endl ;
+//	cout << st.query( 5 , 5 ) << endl ;
+//	cout << st.query( 0 , 9 ) << endl ;
+//	st.modify1(1,5,100);
+//	cout << st.query( 1, 5 ) << endl ;
+//	cout << st.query(8,8) << endl ; 
+//
+//	cout << "*****" << endl ; 
+//	segment_tree< int , Min<int> > st2;
+//
+//	st2.build( a , 0 , 0 , 9 ) ; 
+//	cout << st2.query( 0 , 0 ) << endl ; 
+//	cout << st2.query( 1 , 1 ) << endl ; 
+//	cout << st2.query( 0 , 9 ) << endl ; 
+//	cout << st2.query( 0 , 0 ) << endl ;
+//	cout << st2.query( 5 , 5 ) << endl ;
+//	cout << st2.query( 0 , 9 ) << endl ;
+//	st2.modify1(0,5,100);
+//	cout << st2.query( 0, 5 ) << endl ;
+//	cout << st2.query(8,8) << endl ; 
+//
+//	cout << "*****" << endl ; 
+	segment_tree< int , Plus<int> > st3;
+	st3.build( a , 0 , 0 , 9 ) ; 
+//	cout << st3.query( 0 , 0 ) << endl ;
+//	cout << st3.query( 5 , 5 ) << endl ;
+//	cout << st3.query( 0 , 9 ) << endl ;
+	cout << st3.query(1,5) << endl ; 
+	st3.modify1(1,5,100);
+	cout << st3.query( 0, 5 ) << endl ;
+//	cout << st3.query(8,8) << endl ; 
 
 	return  0;
 }
